4e464918bc69f40bf84d2302cd83832c28c311d2
Huy Phan <HuyPhan0211@gmail.com>
2024-03-27 14:57:06 +0700

    Initial Commit

---
+++ TrainingTabata/TrainingTabataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingTabataApp.swift
+//  TrainingTabata
+//
+//  Created by Huy Phan on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TrainingTabataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
